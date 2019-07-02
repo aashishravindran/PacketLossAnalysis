3cea34d2e666796968c760344a1213ba21a135e6
aashsih2234 <ravindran.aashish@stonybrook.edu>
2019-07-02 09:30:44 -0400

    Added comments

--- get_count.py
+++ get_count.py
@@ -5,67 +5,38 @@ Created on Mon Jul  1 10:07:17 2019
 
 @author: ashubunutu
 """
-
-
-#ata = open('48Mbps_1.txt').read()
-#Ecount = data.count('Starting')
-
-
-
 get_frame_rate=input('Enter Desired Mbps Rate')
 ## Enter frame rate as follows  for 48Mbps please enter 48 
-
 new_count_1=1
 nc=[]
 
-
-print(new_count_1)
-
+#print(new_count_1)
 
 for i in range(1,5):
     makestting=str(get_frame_rate+'Mbps_'+str(i)+'.txt')
     makestting=makestting.lstrip()
+    ## makestring is added to generate the text file to be read dynamically 
     
-    try: 
-        with open(makestting) as f:
-            for line in f:
-                new_count_1+= line.count("Starting")
-        nc.append(new_count_1)
+    try: #Adding try except block to handle "File Not found Exception"
+        with open(makestting) as f: #File Open Statement
+            for line in f: #Iterate through All the lines in the file and look for the Phrase Starting
+                new_count_1+= line.count("Starting") #Count the number of times Starting occurs since each Starting is a new Run 
+        nc.append(new_count_1) # After the entire file is read, append the count to the final array 
         new_count_1=1
-    except (OSError, IOError) as e:
+    except (OSError, IOError) as e: #Any exception raised in try block will be handled here 
         print(str(e))
-        
-#print("Number Of Runs:")
-print(nc)
-
 
 result = False;
 if len(nc) > 0 :
     result = all(elem == nc[0] for elem in nc)
- 
+# To check if all the elements in the array are same  
 if result :
     print("Number of Runs is the same")
+    print("Number Of Runs:"+str(nc)) #Print array with number of runs
 elif OSError or IOError :        
-    print("Some error Occuered")
+    print("Some error Occurred")
 else:
     print("Number of Runs is the diffrent")
 
-# =============================================================================
-# for i in range(1,5):
-#     makestting=str(get_frame_rate+'Mbps_'+str(i)+'.txt')
-#     makestting=makestting.lstrip()
-#     
-#     with open(makestting) as f:
-#         
-#         for line in f:
-#             new_count_1+= line.count("Starting")
-#     nc.append(new_count_1)
-#     new_count_1=1;  
-#     print(makestting.lstrip())
-#     
-# print("Number Of Runs:")
-# print(nc)
-# 
-# =============================================================================
-#
+
 
