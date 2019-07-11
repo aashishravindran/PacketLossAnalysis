eaefc458b542b2535931b6bfab59e8758ee69b07
aashsih2234 <ravindran.aashish@stonybrook.edu>
2019-07-11 15:11:35 -0400

    Time Sync and Receiver Delay Made more Modular

--- imports.py
+++ imports.py
@@ -8,63 +8,46 @@ Created on Thu Jul  4 09:52:50 2019
 
 
 def reference_ts(line):
-    reference_timestamps=[];
-    for i in range(0,len(line)):
-    #print(i,line[i])
-        if i==0:
-           reference_timestamps.append(float(line[i].split(',@')[1])) #Get all time Stamps from the reference file
+    reference_timestamps = [];
+    for i in range(0, len(line)):
+        # print(i,line[i])
+        if i == 0:
+            reference_timestamps.append(float(line[i].split(',@')[1]))  # Get all time Stamps from the reference file
         elif "Starting" in line[i]:
-            i+=1; #skip the line with "Starting new line"
+            i += 1;  # skip the line with "Starting new line"
             reference_timestamps.append(float(line[i].split(',@')[1]))
     return reference_timestamps
-       
-   
- 
-def compute_time_sync(reference_timestamps,new_file,dest_pt):
-     test=open(dest_pt,"w")
-     
-     ref_index=0;
-     #k=0;
-     j=0;
-     length=int(len(new_file)) #Get length of New_file
-     print(length)
 
-     while j < length:
-                        
-              if j==0:
-                  #print("I came Here")
-                  #Time Sync Calcualtion Starts here
-                  new_val=new_file[0].split(",@") #Split to get the time Stamp
-                  count=float("{0:.3f}".format(float(new_val[1])-reference_timestamps[ref_index]))
-                  sync_count=float("{0:.3f}".format(float(new_val[1])-count))
-                  fin_out=str(new_val[0])+", @"+str(sync_count)
-                  test.write(fin_out+"\n")
-                       
-              elif (new_file[j].strip() =="=================Starting New Run========="):
-                          
-                          j+=1;
-                          ref_index+=1
-                          new_val=new_file[j].split(",@")
-                          count=float("{0:.3f}".format(float(new_val[1])-reference_timestamps[ref_index]))
-                          sync_count=float("{0:.3f}".format(float(new_val[1])-count))
-                          fin_out=str(new_val[0])+", @"+str(sync_count)
-                          test.write("=========================Starting New Run==================\n")
-                          test.write(fin_out+"\n")
-                          print(fin_out)
-                          
-                          
-                          
-              else:
-                          #Calculating for other frames from the second frame to "Starting new Run"
-                          new_val_1=new_file[j].split(",@")
-                          final_count=float("{0:.3f}".format(float(new_val_1[1])-sync_count))
-                          T_count=float("{0:.3f}".format(float(new_val_1[1])+final_count))
-                          fin_out_1=str(new_val_1[0])+", @"+str(T_count)
-                          test.write(fin_out_1+"\n")
-                          print("s"+fin_out_1+","+str(j))
 
-              j+=1 
-              
-     test.close() #Close the file
-     return 0;
+def get_retransmission_delays(ref,sync):
+    difference = []
+    delay = {}
+    for i in range(0, len(sync)):
+        for j in range(0, len(sync[i])):
+            # print(sync[i][j])
+            # print(sync[i+1][j])
+            if sync[i][j] in ref[0]:
+                diff = sync[i + 1][j] - ref[1][ref[0].index(sync[i][j])] #calculate Time Difference
+                # print(diff)
+                difference.append(diff)
+                delay[sync[i][j]] = diff ##Append to dictionary with seqnece Number and Delay
+    return delay
+
+
+def time_sync(reference, time_stamps):
+    synchronied_timestamp = []
+    for index,timestamp in enumerate(time_stamps):
+        if index == 0:
+            count=timestamp-reference
+            sync_count = timestamp-count
+            synchronied_timestamp.append(sync_count) #Time Sync Logic
+        else:
+            next_time_stamp = timestamp-sync_count #Time Sync Logic
+            final_count = timestamp+next_time_stamp
+            synchronied_timestamp.append(final_count)
+    return synchronied_timestamp #Retrun the timestamp for each sequence no
+
+
+
+
 
--- main.py
+++ main.py
@@ -6,24 +6,61 @@ Created on Thu Jul  4 19:50:37 2019
 @author: ashubunutu
 """
 
-from imports import reference_ts,compute_time_sync
+from imports import get_retransmission_delays,time_sync,reference_ts
 
 
-file=open("files/ts_logs/time_sync.txt","r") ##Global Reference File
+# =====================================Function Decleration Startes Here ===========================================
 
 
-name=["files/ts_logs/time_sync_1.txt","files/ts_logs/time_sync_2.txt"]
+def parse_tf_and_get_ts(reference_map, file_to_sync):
+    ls = file_to_sync.readlines()
+    seq = []
+    times = []
+    val = []
+    SyncLog = {}
+    count = 0;
+    for i in range(0, len(ls)):
+
+        if "Starting" in ls[i]:
+            reference = reference_map[count]
+            sync = time_sync(reference, times)
+            val = [seq, sync] ## 2D Array containing seq and synchronized time Stamps
+            SyncLog[count] = val #Appending the value to the dictionary for delay calculation
+            seq = [];
+            times = [];
+            count += 1
+
+        else:
+            times.append(float(ls[i].split(',@')[1]))
+            seq.append(int(ls[i].split('and')[0].split('=')[1]))
+
+    return SyncLog
+# Call Transmission Delay and Print Value======================================================
 
 
-#dest="The_Great_gatsBy_logss.txt"
+def execute_delay_fn_print(ref,recv):
+      dictionary = {}
+      for key, value in enumerate(recv):
+            # print(recv_1[key])
+            # break
+            dictionary[key] = get_retransmission_delays(ref[key], recv[key])
+      return dictionary
 
-line=file.readlines()
 
-reference_map=reference_ts(line)
 
-for i,num in enumerate(name,1):
-      fp=open(num,"r")
-      new_file=fp.readlines()
-      dest="files/Sync_logs"+str(i)+".txt"
-      compute_time_sync(reference_map,new_file,dest)
-      fp.close()
+file=open ('files/ts_logs/time_sync.txt','r')
+Sync_log_1 = open('files/ts_logs/time_sync_1.txt','r')
+Sync_log_2 = open('files/ts_logs/time_sync_2.txt','r')
+
+line = file.readlines()
+
+reference_map = reference_ts(line) # Passing the reference file to get time Stamps
+
+ref=parse_tf_and_get_ts(reference_map,Sync_log_1) # Time Sync for Recv 1
+recv_1=parse_tf_and_get_ts(reference_map,Sync_log_2) # Time Sync for Recv 2
+
+receiver_delay=execute_delay_fn_print(ref,recv_1) # Receiver Delay Between Receivers
+
+print(receiver_delay[0])
+
+
