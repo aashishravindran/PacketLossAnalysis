7aeb40a4d7ada010f41e07938e805eb2dda04a51
aashsih2234 <ravindran.aashish@stonybrook.edu>
2019-07-29 10:23:36 -0400

    Some Change

--- vizualizations.py
+++ vizualizations.py
@@ -10,6 +10,7 @@ from get_count import get_count
 import matplotlib.pyplot as plt
 import numpy as np
 from pmf import *
+from probabily_mass_function import *
 
 frame1=24
 interval=1
@@ -153,55 +154,186 @@ plt.title('Node4:Aggregated_Pmf')
 plt.savefig('img/Node_4/Aggregated_pmf.png')
 plt.clf()
 
-#========================Loss Burst Plot==============
-#for i in range(0,len(recv_1)):
-#    loss_burst=get_frame_value(recv_1[i])[2]
-#    aggr_frame_loss=identify_loss_burst_intervals(loss_burst)
-##    probability=probabilty(aggr_frame_loss)
-#    lists=aggr_frame_loss.items()
-#    x, y = zip(*lists)
-#    plt.bar(x,y)
-#    plt.xlabel('Interval', fontsize=18)
-#    plt.ylabel('Loss Burst', fontsize=16)
-#    plt.title('Node1:Loss BurstRun_No'+str(i))
-#    plt.savefig('img/Node_1/Loss BurstRun_No'+str(i)+'.png')
-#    plt.clf()
-#for i in range(0,len(recv_2)):
-#    loss_burst=get_frame_value(recv_2[i])[2]
-#    aggr_frame_loss=identify_loss_burst_intervals(loss_burst)
-#    lists=aggr_frame_loss.items()
-#    x, y = zip(*lists)
-#    plt.bar(x,y)
-#    plt.xlabel('Interval', fontsize=18)
-#    plt.ylabel('Loss Burst', fontsize=16)
-#    plt.title('Node2:Loss BurstRun_No'+str(i))
-#    plt.savefig('img/Node_2/Loss BurstRun_No'+str(i)+'.png')
-#    plt.clf()
-#for i in range(0,len(recv_3)):
-#    loss_burst=get_frame_value(recv_3[i])[2]
-#    aggr_frame_loss=identify_loss_burst_intervals(loss_burst)
-##    probability=probabilty(aggr_frame_loss)
-#    lists=aggr_frame_loss.items()
-#    x, y = zip(*lists)
-#    plt.bar(x,y)
-#    plt.xlabel('Interval', fontsize=18)
-#    plt.ylabel('Loss Burst', fontsize=16)
-#    plt.title('Node3:Loss BurstRun_No'+str(i))
-#    plt.savefig('img/Node_3/Loss BurstRun_No'+str(i)+'.png')
-#    plt.clf()
-#for i in range(0,len(recv_4)):
-#    loss_burst=get_frame_value(recv_4[i])[2]
-#    aggr_frame_loss=identify_loss_burst_intervals(loss_burst)
-##    probability=probabilty(aggr_frame_loss)
-#    lists=aggr_frame_loss.items()
-#    x, y = zip(*lists)
-#    plt.bar(x,y)
-#    plt.xlabel('Interval', fontsize=18)
-#    plt.ylabel('Loss Burst', fontsize=16)
-#    plt.title('Node4:Loss BurstRun_No'+str(i))
-#    plt.savefig('img/Node_4/Loss BurstRun_No'+str(i)+'.png')
-#    plt.clf()
-#
+
+
+
+nrows = 4
+fig, ax = plt.subplots(nrows, 2,figsize=(20,10))
+
+recv=get_allruns(recv_1,1)
+
+
+k = Counter(recv) 
+print(k.most_common(3))
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[0][0].bar(x,y)
+ax[0][0].set_title('Burst Len Vs Pmf across all runs Node 1',fontsize=10,pad=-10)
+
+recv=get_allruns(recv_1,2)
+k = Counter(recv) 
+print(k.most_common(3))
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[0][1].bar(x,y)
+ax[0][1].set_title('Interval Vs Pmf across all runs Node 1',fontsize=10,pad=-10)
+
+recv=get_allruns(recv_2,1)
+k = Counter(recv) 
+print(k.most_common(3))
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[1][0].bar(x,y)
+ax[1][0].set_title('Burst Len Vs Pmf across all runs Node 2',fontsize=10,pad=-10)
+
+
+
+recv=get_allruns(recv_2,2)
+k = Counter(recv) 
+print(k.most_common(3))
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[1][1].bar(x,y)
+ax[1][1].set_title('Interval Vs Pmf across all runs Node 2',fontsize=10,pad=-10)
+
+
+
+recv=get_allruns(recv_3,1)
+k = Counter(recv) 
+print(k.most_common(3))
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[2][0].bar(x,y)
+ax[2][0].set_title('Burst Len Vs Pmf across all runs Node 3',fontsize=10,pad=-10)
+
+recv=get_allruns(recv_3,2)
+k = Counter(recv) 
+print(k.most_common(3))
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[2][1].bar(x,y)
+ax[2][1].set_title('Interval Vs Pmf across all runs Node 3',fontsize=10,pad=-10)
+
+
+recv=get_allruns(recv_4,1)
+k = Counter(recv) 
+print(k.most_common(3))
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[3][0].bar(x,y)
+ax[3][0].set_title('Burst Len Vs Pmf across all runs Node 4',fontsize=10,pad=-10)
+
+recv=get_allruns(recv_4,2)
+
+#ret=consolidated_run(recv,1)
+lists=recv.items()
+x,y=zip(*lists)
+ax[3][1].bar(x,y)
+ax[3][1].set_title('Interval Vs Pmf across all runs Node 4',fontsize=10,pad=-10)
+fig.suptitle('Plot Recv wise aggregation of Burse size and Interval')
+fig.savefig('img/Aggregared.png')
+
+
+
+
+
+
+for i in range(0,len(recv_1)):
+    fig,ax=plt.subplots(nrows=1,ncols=2,sharex=False,sharey=False,squeeze=False)
+    retrun =get_frame_value(recv_1[i])[1]
+    burst_len= loss_burst_pmf(retrun)
+    lists=burst_len.items()
+    x, y = zip(*lists)
+    ax[0][0].bar(x,y)
+    ax[0][0].set_xlabel('Loss Burst Len')
+    ax[0][0].set_ylabel('Pmf')
+#    retrun =get_frame_value(recv_1[i])[1]
+    interval= loss_burst_interval(retrun)
+    lists=interval.items()
+    x, y = zip(*lists)
+    ax[0][1].bar(x,y)
+    ax[0][1].set_xlabel('Interval')
+    ax[0][1].set_ylabel('Pmf')
+    fig.suptitle('Node1:Pmf for Loss Burst Len and Interval for Run_no'+str(i))
+    fig.savefig('img/Node_1/Pmf_Run_no'+str(i)+'.png')
+    fig.clf()
+    plt.close(fig)
+    
+
+
+for i in range(0,len(recv_2)):
+    fig,ax=plt.subplots(nrows=1,ncols=2,sharex=False,sharey=False,squeeze=False)
+    retrun =get_frame_value(recv_2[i])[1]
+    burst_len= loss_burst_pmf(retrun)
+    lists=burst_len.items()
+    x, y = zip(*lists)
+    ax[0][0].bar(x,y)
+    ax[0][0].set_xlabel('Loss Burst Len')
+    ax[0][0].set_ylabel('Pmf')
+#    retrun =get_frame_value(recv_1[i])[1]
+    interval= loss_burst_interval(retrun)
+    lists=interval.items()
+    x, y = zip(*lists)
+    ax[0][1].bar(x,y)
+    ax[0][1].set_xlabel('Interval')
+    ax[0][1].set_ylabel('Pmf')
+    fig.suptitle('Node2:Pmf for Loss Burst Len and Interval for Run_no'+str(i))
+    fig.savefig('img/Node_2/Pmf_Run_no'+str(i)+'.png')
+    fig.clf()
+    plt.close(fig)
+    
+
+for i in range(0,len(recv_3)):
+    x, y = zip(*lists)
+    ax[0][0].bar(x,y)
+    ax[0][0].set_xlabel('Loss Burst Len')
+    ax[0][0].set_ylabel('Pmf')
+#    retrun =get_frame_value(recv_1[i])[1]
+    fig,ax=plt.subplots(nrows=1,ncols=2,sharex=False,sharey=False,squeeze=False)
+    retrun =get_frame_value(recv_3[i])[1]
+    burst_len= loss_burst_pmf(retrun)
+    lists=burst_len.items()
+    x, y = zip(*lists)
+    ax[0][0].bar(x,y)
+    ax[0][0].set_xlabel('Loss Burst Len')
+    ax[0][0].set_ylabel('Pmf')
+#    retrun =get_frame_value(recv_1[i])[1]
+    interval= loss_burst_interval(retrun)
+    lists=interval.items()
+    x, y = zip(*lists)
+    ax[0][1].bar(x,y)
+    ax[0][1].set_xlabel('Interval')
+    ax[0][1].set_ylabel('Pmf')
+    fig.suptitle('Node3:Pmf for Loss Burst Len and Interval for Run_no'+str(i))
+    fig.savefig('img/Node_3/Pmf_Run_no'+str(i)+'.png')
+    fig.clf()
+    plt.close(fig)
+    
+
+for i in range(0,len(recv_4)):
+    fig,ax=plt.subplots(nrows=1,ncols=2,sharex=False,sharey=False,squeeze=False)
+    retrun =get_frame_value(recv_4[i])[1]
+    burst_len= loss_burst_pmf(retrun)
+    lists=burst_len.items()
+    interval= loss_burst_interval(retrun)
+    lists=interval.items()
+    x, y = zip(*lists)
+    ax[0][1].bar(x,y)
+    ax[0][1].set_xlabel('Interval')
+    ax[0][1].set_ylabel('Pmf')
+    fig.suptitle('Node4:Pmf for Loss Burst Len and Interval for Run_no'+str(i))
+    fig.savefig('img/Node_4/Pmf_Run_no'+str(i)+'.png')
+    fig.clf()
+    plt.close(fig)
+    
+
 
 
 
